74ea357ced1d030fd075f7ed71c753fe22b5c4f9
Andrei Khvalko <andrey.greams@gmail.com>
2020-06-01 20:33:41 +0300

    Initial Commit

---
+++ TDEE Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TDEE Calculator
+//
+//  Created by Andrei Khvalko on 6/1/20.
+//  Copyright © 2020 Greams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
