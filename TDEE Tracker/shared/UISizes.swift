02d635477d6aff71fac38e988f45a73b1c2c2f40
Andrei Khvalko <andrey.greams@gmail.com>
2020-12-22 14:26:16 +0300

    WIP UISizes transitional update

--- TDEE Tracker/shared/UISizes.swift
+++ TDEE Tracker/shared/UISizes.swift
@@ -69,7 +69,7 @@ struct UISizes {
     let keyboardButtonHeight: CGFloat
     let keyboardBottomPadding: CGFloat
     
-    public static let current: UISizes = Self.getSizes()
+    public static let current: UISizes = UISizes()
     
     // MARK: - Constants
     
@@ -92,160 +92,113 @@ struct UISizes {
 
     // MARK: - Functions
     
-    private static func getSizes() -> UISizes {
+    init() {
         
-        // NOTE: ~0.46 for iPhones with notch
-        // NOTE: ~0.56 for iPhones without notch
+        // NOTE: 9:19.5 ~0.46 for iPhones with notch
+        // NOTE: 9:16   ~0.56 for iPhones without notch
         let hasNotch: Bool = ( Self.SCREEN_WIDTH_CURRENT / Self.SCREEN_HEIGHT_CURRENT ) < 0.5
-        
-        let widthMultiplier: CGFloat = Self.SCREEN_WIDTH_CURRENT / Self.SCREEN_WIDTH_S
-        let heightMultiplier: CGFloat = Self.SCREEN_HEIGHT_CURRENT / Self.SCREEN_HEIGHT_M
-        
-        Utils.log(source: "getSizes", message: "dimensions = \(Self.SCREEN_WIDTH_CURRENT / Self.SCREEN_HEIGHT_CURRENT)")
-        Utils.log(source: "getSizes", message: "hasNotch = \(hasNotch)")
-        
-        Utils.log(source: "getSizes", message: "widthMultiplier = \(widthMultiplier)")
-        Utils.log(source: "getSizes", message: "heightMultiplier = \(heightMultiplier)")
-        
+
         if hasNotch {
-            return UISizes(
-                
-                welcomeSubTitleVPadding: heightMultiplier * 20,
-                welcomeInputsVSpacing: heightMultiplier * 16,
-                welcomeHintFontSize: heightMultiplier * 22,
-                
-                navbarHeight: heightMultiplier * 84,
-                navbarPadding: heightMultiplier * 12,
-                navbarSpacing: widthMultiplier * 54,
+            
+            let widthMultiplier: CGFloat = Self.SCREEN_WIDTH_CURRENT / Self.SCREEN_WIDTH_S
+            let heightMultiplier: CGFloat = Self.SCREEN_HEIGHT_CURRENT / Self.SCREEN_HEIGHT_M
+            
+            let scale = ( widthMultiplier + heightMultiplier ) / 2
 
-                calendarMonthFontSize: widthMultiplier * 20,
-                calendarMonthButtonHeight: heightMultiplier * 44,
-                calendarDayFontSize: heightMultiplier * 24,
-                calendarDayButton: heightMultiplier * 44,
-                
-                entryInputBaseSize: heightMultiplier * 36,
-                entryInputPadding: heightMultiplier * 27,
-                entryHintBlockPadding: heightMultiplier * 16,
-                entryBlockerIconPadding: heightMultiplier * 75,
-                
-                entryHintFontSize: widthMultiplier * 17,
-                entryHintLabelFontSize: widthMultiplier * 15,
-                entryHintHPadding: widthMultiplier * 30,
-                
-                trendsElementPadding: heightMultiplier * 10,
-                trendsItemLabelFontSize: heightMultiplier * 18,
-                trendsItemValueFontSize: heightMultiplier * 32,
-                trendsItemUnitFontSize: heightMultiplier * 14,
-                
-                progressPageSpacing: heightMultiplier * 32,
-                progressChartHeight: heightMultiplier * 180,
-                progressCircleDiameter: heightMultiplier * 340,
-                progressCircleWidth: heightMultiplier * 40,
-                
-                setupDefaultButtonHeight: heightMultiplier * 44,
-                setupInputHeight: heightMultiplier * 74,
-                setupInputLabelFontSize: heightMultiplier * 18,
-                setupTargetDeltaPadding: heightMultiplier * 12,
-                targetDeltaHPadding: widthMultiplier * 28,
-                
-                keyboardButtonWidth: widthMultiplier * 120,
-                keyboardButtonHeight: heightMultiplier * 50,
-                keyboardBottomPadding: heightMultiplier * 40
-            )
-        }
-        else {
-            return UISizes(
-                
-                welcomeSubTitleVPadding: getSizeByHeight(xxl: 40, xl: 40, l: 20, m: 20, s: 20, xs: 20, xxs: 4),
-                welcomeInputsVSpacing: getSizeByHeight(xxl: 16, xl: 16, l: 16, m: 16, s: 16, xs: 16, xxs: 4),
-                welcomeHintFontSize: getSizeByHeight(xxl: 24, xl: 24, l: 22, m: 22, s: 22, xs: 22, xxs: 18),
-                
-                navbarHeight: getSizeByHeight(xxl: 84, xl: 84, l: 84, m: 84, s: 60, xs: 60, xxs: 56),
-                navbarPadding: getSizeByHeight(xxl: 12, xl: 12, l: 12, m: 12, s: 8, xs: 8, xxs: 6),
-                navbarSpacing: getSizeByWidth(xl: 62, l: 62, m: 54, s: 54, xs: 42),
+            // Self.SCREEN_WIDTH_S x Self.SCREEN_HEIGHT_M
 
-                calendarMonthFontSize: getSizeByWidth( xl: 24, l: 24, m: 24, s: 24, xs: 16),
-                calendarMonthButtonHeight: getSizeByHeight(xxl: 44, xl: 44, l: 44, m: 44, s: 44, xs: 44, xxs: 36),
-                calendarDayFontSize: getSizeByHeight(xxl: 24, xl: 24, l: 24, m: 24, s: 24, xs: 19, xxs: 18),
-                calendarDayButton: getSizeByHeight(xxl: 48, xl: 46, l: 44, m: 44, s: 44, xs: 39, xxs: 31),
-                
-                entryInputBaseSize: getSizeByHeight(xxl: 40, xl: 40, l: 36, m: 36, s: 36, xs: 36, xxs: 30),
-                entryInputPadding: getSizeByHeight(xxl: 39, xl: 39, l: 32, m: 27, s: 22, xs: 16, xxs: 14),
-                entryHintBlockPadding: getSizeByHeight(xxl: 20, xl: 20, l: 16, m: 16, s: 10, xs: 6, xxs: 0),
-                entryBlockerIconPadding: getSizeByHeight(xxl: 86, xl: 86, l: 75, m: 75, s: 52, xs: 47, xxs: 47),
-                
-                entryHintFontSize: getSizeByWidth(xl: 18, l: 17, m: 17, s: 17, xs: 14),
-                entryHintLabelFontSize: getSizeByWidth(xl: 18, l: 17, m: 15, s: 15, xs: 12),
-                entryHintHPadding: getSizeByWidth(xl: 34, l: 34, m: 34, s: 30, xs: 28),
-                
-                trendsElementPadding: getSizeByHeight(xxl: 17, xl: 17, l: 14, m: 10, s: 7, xs: 3, xxs: 2),
-                trendsItemLabelFontSize: getSizeByHeight(xxl: 20, xl: 20, l: 18, m: 18, s: 18, xs: 18, xxs: 12),
-                trendsItemValueFontSize: getSizeByHeight(xxl: 34, xl: 34, l: 32, m: 32, s: 32, xs: 32, xxs: 28),
-                trendsItemUnitFontSize: getSizeByHeight(xxl: 16, xl: 16, l: 14, m: 14, s: 14, xs: 14, xxs: 12),
-                
-                progressPageSpacing: getSizeByHeight(xxl: 38, xl: 34, l: 34, m: 32, s: 16, xs: 8, xxs: 2),
-                progressChartHeight: getSizeByHeight(xxl: 210, xl: 210, l: 180, m: 180, s: 180, xs: 160, xxs: 120),
-                progressCircleDiameter: getSizeByHeight(xxl: 380, xl: 380, l: 340, m: 340, s: 340, xs: 310, xxs: 272),
-                progressCircleWidth: getSizeByHeight(xxl: 56, xl: 56, l: 40, m: 40, s: 40, xs: 40, xxs: 30),
-                
-                setupDefaultButtonHeight: getSizeByHeight(xxl: 44, xl: 44, l: 44, m: 44, s: 44, xs: 44, xxs: 40),
-                setupInputHeight: getSizeByHeight(xxl: 74, xl: 74, l: 74, m: 74, s: 68, xs: 58, xxs: 58),
-                setupInputLabelFontSize: getSizeByHeight(xxl: 18, xl: 18, l: 18, m: 18, s: 18, xs: 18, xxs: 14),
-                setupTargetDeltaPadding: getSizeByHeight(xxl: 12, xl: 12, l: 12, m: 12, s: 12, xs: 8, xxs: 8),
-                targetDeltaHPadding: getSizeByWidth(xl: 40, l: 40, m: 32, s: 28, xs: 20),
-                
-                keyboardButtonWidth: getSizeByWidth(xl: 132, l: 132, m: 120, s: 120, xs: 102),
-                keyboardButtonHeight: getSizeByHeight(xxl: 50, xl: 50, l: 50, m: 50, s: 50, xs: 50, xxs: 40),
-                keyboardBottomPadding: getSizeByHeight(xxl: 40, xl: 40, l: 40, m: 40, s: 8, xs: 6, xxs: 6)
-            )
-        }
-    }
-    
-    private static func getSizeByHeight(
-        xxl: CGFloat, xl: CGFloat, l: CGFloat, m: CGFloat, s: CGFloat, xs: CGFloat, xxs: CGFloat
-    ) -> CGFloat {
-        
-        if SCREEN_HEIGHT_CURRENT >= SCREEN_HEIGHT_XXL {
-            return xxl
-        }
-        else if SCREEN_HEIGHT_CURRENT >= SCREEN_HEIGHT_XL {
-            return xl
-        }
-        else if SCREEN_HEIGHT_CURRENT >= SCREEN_HEIGHT_L {
-            return l
-        }
-        else if SCREEN_HEIGHT_CURRENT >= SCREEN_HEIGHT_M {
-            return m
-        }
-        else if SCREEN_HEIGHT_CURRENT >= SCREEN_HEIGHT_S {
-            return s
-        }
-        else if SCREEN_HEIGHT_CURRENT >= SCREEN_HEIGHT_XS {
-            return xs
+            self.welcomeSubTitleVPadding = scale * 20
+            self.welcomeInputsVSpacing = scale * 16
+            self.welcomeHintFontSize = scale * 22
+
+            self.navbarHeight = scale * 84
+            self.navbarPadding = scale * 12
+            self.navbarSpacing = scale * 54
+
+            self.calendarMonthFontSize = scale * 20
+            self.calendarMonthButtonHeight = scale * 44
+            self.calendarDayFontSize = scale * 24
+            self.calendarDayButton = scale * 44
+
+            self.entryInputBaseSize = scale * 36
+            self.entryInputPadding = scale * 27
+            self.entryHintBlockPadding = scale * 16
+            self.entryBlockerIconPadding = scale * 75
+
+            self.entryHintFontSize = scale * 17
+            self.entryHintLabelFontSize = scale * 15
+            self.entryHintHPadding = scale * 30
+
+            self.trendsElementPadding = scale * 10
+            self.trendsItemLabelFontSize = scale * 18
+            self.trendsItemValueFontSize = scale * 32
+            self.trendsItemUnitFontSize = scale * 14
+
+            self.progressPageSpacing = scale * 32
+            self.progressChartHeight = scale * 180
+            self.progressCircleDiameter = scale * 340
+            self.progressCircleWidth = scale * 40
+
+            self.setupDefaultButtonHeight = scale * 44
+            self.setupInputHeight = scale * 74
+            self.setupInputLabelFontSize = scale * 18
+            self.setupTargetDeltaPadding = scale * 12
+            self.targetDeltaHPadding = scale * 28
+
+            self.keyboardButtonWidth = scale * 120
+            self.keyboardButtonHeight = scale * 50
+            self.keyboardBottomPadding = scale * 40
         }
         else {
-            return xxs
-        }
-    }
-    
-    private static func getSizeByWidth(
-        xl: CGFloat, l: CGFloat, m: CGFloat, s: CGFloat, xs: CGFloat
-    ) -> CGFloat {
-        
-        if SCREEN_WIDTH_CURRENT >= SCREEN_WIDTH_XL {
-            return xl
-        }
-        else if SCREEN_WIDTH_CURRENT >= SCREEN_WIDTH_L {
-            return l
-        }
-        else if SCREEN_WIDTH_CURRENT >= SCREEN_WIDTH_M {
-            return m
-        }
-        else if SCREEN_WIDTH_CURRENT >= SCREEN_WIDTH_S {
-            return s
-        }
-        else {
-            return xs
+
+            let widthMultiplier: CGFloat = Self.SCREEN_WIDTH_CURRENT / Self.SCREEN_WIDTH_XS
+            let heightMultiplier: CGFloat = Self.SCREEN_HEIGHT_CURRENT / Self.SCREEN_HEIGHT_XXS
+            
+            let scale = ( widthMultiplier + heightMultiplier ) / 2
+            
+            // Self.SCREEN_WIDTH_XS x Self.SCREEN_HEIGHT_XXS
+
+            self.welcomeSubTitleVPadding = scale * 4
+            self.welcomeInputsVSpacing = scale * 4
+            self.welcomeHintFontSize = scale * 18
+
+            self.navbarHeight = scale * 56
+            self.navbarPadding = scale * 6
+            self.navbarSpacing = scale * 42
+
+            self.calendarMonthFontSize = scale * 16
+            self.calendarMonthButtonHeight = scale * 36
+            self.calendarDayFontSize = scale * 18
+            self.calendarDayButton = scale * 31
+
+            self.entryInputBaseSize = scale * 30
+            self.entryInputPadding = scale * 14
+            self.entryHintBlockPadding = scale * 0
+            self.entryBlockerIconPadding = scale * 47
+
+            self.entryHintFontSize = scale * 14
+            self.entryHintLabelFontSize = scale * 12
+            self.entryHintHPadding = scale * 28
+
+            self.trendsElementPadding = scale * 2
+            self.trendsItemLabelFontSize = scale * 12
+            self.trendsItemValueFontSize = scale * 28
+            self.trendsItemUnitFontSize = scale * 12
+
+            self.progressPageSpacing = scale * 2
+            self.progressChartHeight = scale * 120
+            self.progressCircleDiameter = scale * 272
+            self.progressCircleWidth = scale * 30
+
+            self.setupDefaultButtonHeight = scale * 40
+            self.setupInputHeight = scale * 58
+            self.setupInputLabelFontSize = scale * 14
+            self.setupTargetDeltaPadding = scale * 8
+            self.targetDeltaHPadding = scale * 20
+
+            self.keyboardButtonWidth = scale * 102
+            self.keyboardButtonHeight = scale * 40
+            self.keyboardBottomPadding = scale * 6
         }
     }
 }
